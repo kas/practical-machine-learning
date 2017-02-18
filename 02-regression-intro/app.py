52e560a89fd3e592b97ae3dcbd03ccc4d3383ffb
Kenneth Schnall <kas6570@psu.edu>
2017-02-18 00:39:12 -0500

    Fix HL_PCT in video 02

--- 02-regression-intro/app.py
+++ 02-regression-intro/app.py
@@ -3,7 +3,7 @@ import quandl
 
 df = quandl.get('WIKI/GOOGL') # pandas dataframe
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume',]]
-df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
